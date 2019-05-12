7f41c4c72c9cd6515b379905622fc841f8da6a4d
Nontawat Tantavanich <tbn_nontawat@hotmail.com>
2019-05-12 21:07:24 +0700

    commit

--- js/login.js
+++ js/login.js
@@ -1,33 +1,30 @@
 $(function(){ 
     $('#InputEmail').focus();
     $('#login').click(function(){
-        $('#loginForm').submit(function (event) {
-            var form = $('#loginForm')[0];
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-          }
-          $(this).addClass('was-validated');
-        });
         login();
-    }
-    );
-
-    function chpage(){
+        $('#alert').clear();
+    });
+    function chpage(check){
         window.location.href='dashboard.html';
     }
-
     function login(){
-    //     var email=$('#InputEmail').val();
-    //     var password=$('#InputPassword').val();
-    //     if (email=='admin@nw.com' && password=='12345**'){
-    //         console.log('pass');
-    //         $('#alert').remove();
-    //         chpage();
-    //     }
-    //     else{
-    //         text='Username or password is not correct!!<br>Please try again.'
-    //         $('#alert').append(text);
-    //     }
-    chpage();
+        var email=$('#InputEmail').val();
+        var password=$('#InputPassword').val();
+        if(!(email.localeCompare('admin@nw.com'))){
+            if(!(password.localeCompare('12345**'))){
+                chpage();
+            }
+            else{
+                console.log('false');
+                text='Username or password is not correct!!<br>Please try again.'
+                $('#alert').append(text);
+            }
+        }
+        else{
+            console.log('false');
+            text='Username or password is not correct!!<br>Please try again.'
+            $('#alert').append(text);
+        }
     }
+    
 });
