68884551cc501369cf81b025fcba876459c161ff
Nontawat Tantavanich <tbn_nontawat@hotmail.com>
2019-05-13 00:05:50 +0700

    login

--- js/login.js
+++ js/login.js
@@ -2,29 +2,17 @@ $(function(){
     $('#InputEmail').focus();
     $('#login').click(function(){
         login();
-        $('#alert').clear();
     });
-    function chpage(check){
+    function chpage(){
         window.location.href='dashboard.html';
     }
     function login(){
         var email=$('#InputEmail').val();
         var password=$('#InputPassword').val();
         if(!(email.localeCompare('admin@nw.com'))){
-            if(!(password.localeCompare('12345**'))){
+            if(!(password.localeCompare('12345**')&&password.localeCompare(''))){
                 chpage();
             }
-            else{
-                console.log('false');
-                text='Username or password is not correct!!<br>Please try again.'
-                $('#alert').append(text);
-            }
-        }
-        else{
-            console.log('false');
-            text='Username or password is not correct!!<br>Please try again.'
-            $('#alert').append(text);
         }
     }
-    
 });
