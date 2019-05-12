26736d6e3dc392c608008072b247838405596be8
Nontawat Tantavanich <tbn_nontawat@hotmail.com>
2019-05-13 02:24:37 +0700

    bug

--- js/customer-list.js
+++ js/customer-list.js
@@ -15,7 +15,9 @@ $(function(){
         var data = table.row( this ).data();
         var value=data.customerID;
         document.cookie=value;
-        window.location.href='cusdetail.html';
+        console.log(document.cookie);
+        
+        // window.location.href='cusdetail.html';
     } );
 });
 
